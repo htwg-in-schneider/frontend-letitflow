5d09549dc20732bbc43cdb7cfc668320ca0515b0
Silvia Brandl <silvia.brandl@htwg-konstanz.de>
2025-11-26 12:58:32 +0100

    Interation 0 und 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// deine alten Styles
+
 import './assets/css/header_style.css'
 import './assets/css/body_style.css'
 import './assets/css/footer_style.css'
