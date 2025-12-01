e8fd6f160c17ccc3799ac1e30336d1a642133935
Selin Bartocha <bartocha.selin03@gmail.com>
2025-12-01 14:46:59 +0100

    Herunterladen und einrichten von tailwind.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import './style.css'
 
-
-import './assets/css/header_style.css'
-import './assets/css/body_style.css'
-import './assets/css/footer_style.css'
-
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
