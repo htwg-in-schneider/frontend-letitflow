23b8e10592e48f41f5b1962db38115e1b9ca3a4d
Selin Bartocha <bartocha.selin03@gmail.com>
2026-01-08 16:27:18 +0100

    Pinia hinzugefügt und in die App integriert

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './assets/style.css'
 
-createApp(App).use(router).mount('#app')
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
