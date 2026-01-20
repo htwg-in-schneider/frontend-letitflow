3fc950bb59350eccd3bfd55415c7abfc78ee661d
Silvi <silvi@student.htwg-konstanz.de>
2026-01-20 19:40:18 +0100

    localhost im fetch ausgetauscht

--- src/pages/OrderView.vue
+++ src/pages/OrderView.vue
@@ -151,7 +151,7 @@ const finishOrder = async () => {
     success('Bestellung abgeschlossen!');
 
     try {
-      await fetch('http://localhost:8081/api/cart', {
+      await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/cart`, {
         method: 'DELETE',
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('auth0_token')}`
